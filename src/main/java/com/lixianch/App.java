ae86a963de38e47e26e63b06a6dfa615caff706c
李贤昌 <xianchang.li@dmall.com>
2016-10-28 16:59:27 +0800

    init

---
+++ src/main/java/com/lixianch/App.java
@@ -0,0 +1,13 @@
+package com.lixianch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
